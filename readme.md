e4ecafac9d1f92becc490e75b8058247b14e99ea
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-14 14:25:56 +0200

    (Style) First version of the grid and display of text and image

---
+++ readme.md
@@ -0,0 +1,3 @@
+<!-- Commande pour compiler -->
+g++ -I src/include -L src/lib -o main.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image
+
---
+++ src/include/Element.hpp
@@ -0,0 +1,10 @@
+#ifndef ELEMENT_HPP
+#define ELEMENT_HPP
+
+#include <SDL2/SDL.h>
+
+// Colors
+const SDL_Color COLOR_BLACK = {0, 0, 0, 255};
+const SDL_Color COLOR_WHITE = {255, 255, 255, 255};
+const SDL_Color COLOR_BLUE = {50, 100, 150, 255};
+#endif
